534f40af03188e2cd83a5f20e95bfb50fa6e4148
Darylabrador <abrador.daryl@gmail.com>
2020-10-25 21:44:55 +0400

    rework : env variables

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
-    host: 'localhost',
+    host: process.env.DB_URL,
     dialect: 'mysql',
     logging: false
 });
